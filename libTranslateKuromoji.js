129d885237031695b84a7595f4a550acd69bd165
royyu95 <roypopo@outlook.com>
2025-09-04 14:07:38 +0800

    kuromoji completed

--- libTranslateKuromoji.js
+++ libTranslateKuromoji.js
@@ -2,53 +2,46 @@
 // tl=en
 (function () {
     var sourceLanguage, targetLanguage;
-
-    // Kuromoji tokenizer loader
-    let kuromojiReady = null;
-    let tokenizer = null;
-
-    async function tokenize(str) {
-        await loadKuromoji();
-        if (!tokenizer) throw new Error('Kuromoji tokenizer not loaded');
-        return tokenizer.tokenize(str);
-    }
-
+    var kuromoji, tokenizer;
     function init(sl, tl) {
+        kuromoji = require("./kuromoji.js");
+        kuromoji.builder({ dicPath: "https://cdn.jsdelivr.net/npm/kuromoji@0.1.2/dict/" }).build(function (err, builttokenizer) {
+            // tokenizer is ready
+            tokenizer = builttokenizer;
+        });
         setSrc(sl);
         setDst(tl);
     }
 
-    // mode: "translate" (default) or "tokenize"
-    async function translate(str, sl, tl) {
-        await loadKuromoji();
-        return tokenize(str);
+    function translate(str, sl, tl) {
+        return createRequest(str, sl, tl);
     }
 
     function createRequest(str, sl, tl) {
-        return fetch("http://localhost:11434/api/generate", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                model: "qwen3:8b",
-                prompt: promptPrefix + str,
-                stream: false,
-                think: false
-            })
+        var result = tokenizer.tokenize(str);
+        // console.log(result);
+        var resultString = "";
+        result.forEach(function (element) {
+            if (element.reading)
+                resultString += element.reading + " ";
+            else
+                resultString += element.surface_form + " ";
         });
+        return Promise.resolve(resultString);
     }
 
     function setSrc(v) {
         sourceLanguage = typeof v === 'string' ? v : 'ja';
     }
+
     function setDst(v) {
         targetLanguage = (typeof v === 'string' && v !== 'auto') ? v : 'en';
     }
 
     if (typeof module !== 'undefined') {
         module.exports = { init, translate, setSrc, setDst, createRequest };
-    } else {
+    }
+    else {
         init();
         globalThis._init = init;
         globalThis._trans = translate;
